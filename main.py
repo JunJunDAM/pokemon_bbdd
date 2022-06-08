35f760eca9d7e92e91312f043b1633ff673739af
JunJunDAM <juninfo1997@gmail.com>
2022-06-08 18:10:02 +0200

    added battle

--- main.py
+++ main.py
@@ -67,16 +67,24 @@ def get_pokemon(pokemon):
 #ADD POKEMON TO DATABASE
 def create_pokemon():
     import random
+    pokemon_type_list = ""
+    random_type = random.randint(0, 30)
+    if random_type < 10:
+        pokemon_type_list = "fuego"
+    elif random_type >= 10 and random_type < 20:
+        pokemon_type_list = "agua"
+    elif random_type >= 20 and random_type <= 30:
+        pokemon_type_list = "planta"
     #POKEMON DATA
     pokemon_name = input("Nombre: ")
     if exists(pokemon_name) == False:
-        pokemon_type = input("Tipo: ")
+        pokemon_type = pokemon_type_list
         #STATS DATA
         atack = random.randint(5, 100)
         special_atack = random.randint(5, 100)
         defense = random.randint(5, 100)
         special_defense = random.randint(5, 100)
-            
+         
         media = (atack+special_atack+defense+special_defense)/4
         #ADD POKEMON TO POKEDEX
         new_pokemon_query = (f"insert into pokedex(pokemon,type) values ('{pokemon_name}','{pokemon_type}')")
@@ -86,6 +94,7 @@ def create_pokemon():
         #ADD STATS
         new_stats_query = f"insert into stats (id_pokemon,atack,special_atack,defense,special_defense,media) values ({pokemon_id},{atack},{special_atack},{defense},{special_defense},{media})"
         database(new_stats_query)
+        get_pokemon(pokemon_name)
     else:
         print("\n" + pokemon_name + " ya esta registrado en la pokedex \n")
  
@@ -95,8 +104,10 @@ def delete_pokemon(pokemon):
     if id_pokemon == False:
         print("\n" + pokemon + " no existe en la pokedex \n")
     else:
-        query = (f"delete from pokedex where id = {id_pokemon}")
-        database(query)
+        delete_stats_query = (f"delete from stats where id_pokemon = {id_pokemon}")
+        delete_pokedex_query = (f"delete from pokedex where id = {id_pokemon}")
+        database(delete_stats_query)
+        database(delete_pokedex_query)
         print("\n" + pokemon + " ha sido eliminado de la pokedex \n")
  
 #UPDATE POKEMON VALUES AND STATS
@@ -122,6 +133,72 @@ def update_pokemon(pokemon):
         get_pokemon(evo_name)
         print("*************************************")
         
+def figth(pokemon1,pokemon2):
+    import random
+    cont_pokemon1 = 0
+    cont_pokemon2 = 0
+    id_pokemon1 = exists(pokemon1)
+    id_pokemon2 = exists(pokemon2)
+    #GET MEDIA
+    media_pokemon1_query = f"select media from stats where id_pokemon = {id_pokemon1}"
+    media_pokemon2_query = f"select media from stats where id_pokemon = {id_pokemon2}"
+    media_pokemon1 = database(media_pokemon1_query)
+    media_pokemon2 = database(media_pokemon2_query)
+    media_pokemon1 = media_pokemon1[0][0]
+    media_pokemon2 = media_pokemon2[0][0]
+    
+    media_dif = abs(media_pokemon1 - media_pokemon2)
+    print(media_dif)
+    max_media = ""
+    min_media = ""
+    if media_pokemon1 > media_pokemon2:
+        max_media = pokemon1
+        min_media = pokemon2
+    else:
+        max_media = pokemon2
+        min_media = pokemon1
+        
+    print("\n")
+    print("****************************************")
+    print("***** " + pokemon1 + " tiene un poder de", media_pokemon1)
+    print("**************    VS    ****************")
+    print("***** " + pokemon2 + " tiene un poder de", media_pokemon2)
+    print("****************************************")
+    print("\n")
+    
+    for i in range(1,4):
+        if media_dif < 10:
+            randomn = random.randint(0, 100)
+            if randomn >= 50:
+                print("Combate", i , ", Ganador: " + max_media)
+                cont_pokemon1 += 1
+            else:
+                print("Combate", i , ", Ganador: " + min_media)
+                cont_pokemon2 += 1
+                
+        elif media_dif >= 10 and media_dif < 30:
+            randomn = random.randint(20, 100)
+            if randomn >= 50:
+                print("Combate", i , ", Ganador: " + max_media)
+                cont_pokemon1 += 1
+            else:
+                print("Combate", i , ", Ganador: " + min_media)
+                cont_pokemon2 += 1
+                
+        elif media_dif >= 30:
+            randomn = random.randint(40, 100)
+            if randomn >= 50:
+                print("Combate", i , ", Ganador: " + max_media)
+                cont_pokemon1 += 1
+            else: 
+                print("Combate", i , ", Ganador: " + min_media)
+                cont_pokemon2 += 1
+    
+    print("\n",max_media, ":" , cont_pokemon1, min_media, ":", cont_pokemon2)
+    if cont_pokemon1 > cont_pokemon2:
+        print("\nGanador : "+ max_media)
+    else:
+        print("\nGanador: " + min_media)
     
 def menu():
     print("\n")
@@ -133,7 +210,8 @@ def menu():
     print("* [2] - Añadir pokemon      *")
     print("* [3] - Eliminar pokemon    *")
     print("* [4] - Evolucionar pokemon *")
-    print("* [5] - Salir               *")
+    print("* [5] - Pelea pokemon       *")
+    print("* [6] - Salir               *")
     print("*****************************")
     option = input("* Opcion: ")
     validar_option(option)
@@ -153,6 +231,10 @@ def validar_option(option):
         pokemon = input("Que pokemon quieres evolucionar? ")
         update_pokemon(pokemon)
     elif option == '5':
+        pokemon = input("Pokemon 1: ")
+        pokemon2 = input("Pokemon 2: ")
+        figth(pokemon,pokemon2)
+    elif option == '6':
         print("\nAdios!")
         exit()
     else: 
